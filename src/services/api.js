c97e710aa4ca24e1a5b4880b32ffa29ea776f8cb
villva-vinoth <vinothpukal6580@gmail.com>
2024-03-05 12:50:57 +0530

    march 05 B

---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+export const BASE_URL = `http://192.168.29.174:3001/api/`;
+
+export const ADD_USER = BASE_URL +`users/register`;
+
+export const LOGIN = BASE_URL +`users/login`;
+
+//token 
+// users details 
+export const GET_USER = BASE_URL +`users/getUserDetailById`; 
